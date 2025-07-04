100c7a0f79c14c28cd5dcc482aa80547278e5d39
unes <assiidi52@gmail.com>
2025-07-04 11:41:11 +0100

    Changes to be committed: 	new file:   addons/estate/__init__.py 	new file:   addons/estate/__manifest__.py 	new file:   addons/estate/__pycache__/__init__.cpython-312.pyc 	new file:   addons/estate/models/__init__.py 	new file:   addons/estate/models/__pycache__/__init__.cpython-312.pyc 	new file:   addons/estate/models/__pycache__/estate_property.cpython-312.pyc 	new file:   addons/estate/models/__pycache__/estate_property_offer.cpython-312.pyc 	new file:   addons/estate/models/__pycache__/estate_property_tag.cpython-312.pyc 	new file:   addons/estate/models/__pycache__/estate_property_type.cpython-312.pyc 	new file:   addons/estate/models/estate_property.py 	new file:   addons/estate/models/estate_property_offer.py 	new file:   addons/estate/models/estate_property_tag.py 	new file:   addons/estate/models/estate_property_type.py 	new file:   addons/estate/security/ir.model.access.csv 	new file:   addons/estate/views/estate__property_offer.xml 	new file:   addons/estate/views/estate_menus.xml 	new file:   addons/estate/views/estate_property_type_views.xml 	new file:   addons/estate/views/estate_property_views.xml 	deleted:    addons/readme.md 	modified:   docker-compose.yml

---
+++ addons/estate/models/estate_property.py
@@ -0,0 +1,58 @@
+from odoo import models, fields
+from datetime import timedelta, date
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Property"
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(
+        copy=False,
+        default=lambda self: date.today() + timedelta(days=90)  # 3 months later
+    )
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(
+        readonly=True,
+        copy=False
+    )
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Orientation',
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ]
+    )
+
+    # Reserved field
+    active = fields.Boolean(default=False)
+
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
+
+    # State field
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled'),
+        ],
+        required=True,
+        copy=False,
+        default='new'
+    )
