9393ccda38b78510d3dc3bd664eddb015d7fdc84
unes <assiidi52@gmail.com>
2025-07-07 11:15:21 +0100

    Changes to be committed: 	modified:   addons/estate/__manifest__.py 	modified:   addons/estate/models/__pycache__/estate_property.cpython-312.pyc 	modified:   addons/estate/models/__pycache__/estate_property_offer.cpython-312.pyc 	modified:   addons/estate/models/__pycache__/estate_property_tag.cpython-312.pyc 	modified:   addons/estate/models/__pycache__/estate_property_type.cpython-312.pyc 	deleted:    addons/estate/views/estate__property_offer.xml

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -7,7 +7,6 @@
     'description': 'Real Estate Property Management',
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_property_offer.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_views.xml',
