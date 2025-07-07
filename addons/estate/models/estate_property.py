aaad89e3550e6375a04f2a694236e1e151a810ef
unes <assiidi52@gmail.com>
2025-07-07 10:54:28 +0100

    Changes to be committed: 	modified:   addons/estate/__manifest__.py 	modified:   addons/estate/models/estate_property.py 	modified:   addons/estate/models/estate_property_offer.py 	modified:   addons/estate/models/estate_property_tag.py 	modified:   addons/estate/models/estate_property_type.py 	modified:   addons/estate/views/estate_menus.xml 	new file:   addons/estate/views/estate_property_tag_views.xml 	modified:   addons/estate/views/estate_property_type_views.xml 	modified:   addons/estate/views/estate_property_views.xml

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,25 +1,15 @@
-from odoo import models, fields
-from datetime import timedelta, date
-
+from odoo import models, fields, api
 
 class EstateProperty(models.Model):
-    _name = "estate.property"
-    _description = "Estate Property"
-    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
-    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
-
+    _name = 'estate.property'
+    _description = 'Real Estate Property'
+    
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(
-        copy=False,
-        default=lambda self: date.today() + timedelta(days=90)  # 3 months later
-    )
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float(
-        readonly=True,
-        copy=False
-    )
+    selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
@@ -27,32 +17,24 @@ class EstateProperty(models.Model):
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Orientation',
-        selection=[
-            ('north', 'North'),
-            ('south', 'South'),
-            ('east', 'East'),
-            ('west', 'West')
-        ]
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
-
-    # Reserved field
-    active = fields.Boolean(default=False)
-
-    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
-    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
-    salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
-
-    # State field
+    active = fields.Boolean(default=True)
     state = fields.Selection(
         selection=[
             ('new', 'New'),
             ('offer_received', 'Offer Received'),
             ('offer_accepted', 'Offer Accepted'),
             ('sold', 'Sold'),
-            ('cancelled', 'Cancelled'),
+            ('canceled', 'Canceled'),
         ],
         required=True,
-        copy=False,
-        default='new'
+        default='new',
+        copy=False
     )
+    
+    property_type_id = fields.Many2one('estate.property.type', string='Property Type')
+    buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False)
+    salesperson_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
+    tag_ids = fields.Many2many('estate.property.tag', string='Tags')
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
